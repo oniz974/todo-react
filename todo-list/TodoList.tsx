ac5a1a5e42ea7d2267e8194f13437ee42b717b54
oniz974 <68774003+oniz974@users.noreply.github.com>
2022-05-17 16:06:34 +0200

    master

---
+++ todo-list/TodoList.tsx
@@ -0,0 +1,9 @@
+import * as UI from "../shared/ui";
+
+/**
+ * Ce fichier contient le composant de l'écran
+ * de la liste des todos
+ */
+export default function TodoList() {
+  return <UI.MyParagraph>Liste des todos</UI.MyParagraph>;
+}
