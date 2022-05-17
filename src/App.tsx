887a8f777516d30e7e7b185a4308eb7b45193b1e
oniz974 <68774003+oniz974@users.noreply.github.com>
2022-05-17 16:26:01 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-/*
+import { GlobalStyle } from "./shared/ui";
+import TodoList from "./todo-list/TodoList";
+
+/**
  * Ce fichier contient le composant principal
- * qui lui même contient l'application
+ * qui lui même contient toute l'application.
  */
 export default function App() {
-  return <h1>TodoList</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <TodoList />
+    </>
+  );
 }
---
+++ src/shared/ui.tsx
@@ -0,0 +1,26 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+/**
+ * Ce fichier contient l'intégralité de nos composants
+ * stylisé. Vous retrouverez TOUT le css de l'application
+ * en dessous
+ */
+
+// Style Globale : Style des balises HTML nue (sans classe)
+export const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito', sans-serif;
+  }
+  h1 {
+    font-family: 'Lobster', sans-serif;
+  }
+`;
+
+// POUR LE FUN
+
+export const MyParagraph = styled.p`
+  text-align: center;
+  color: #725498;
+`;
